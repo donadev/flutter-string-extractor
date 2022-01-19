6501f40aaee3414bc90ebca4237df4e4565ce64e
Luigi Donadel <luigi.donadel@gmail.com>
2022-01-19 11:39:48 +0100

    adds dotenv support

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs/promises';
+import 'dotenv/config'
+
 import extractor from './lib/extract';
 import { question } from './lib/util/utils';
 
@@ -6,9 +8,11 @@ import { question } from './lib/util/utils';
 
 
 const start = async () => {
-    const folder : string = "/Users/donadev/Documents/Projects/AmmessoApp/lib"//await question("Insert the main folder where to search for strings: ")
-    const excluded_paths : string[]  = ["/Users/donadev/Documents/Projects/AmmessoApp/lib/amplifyconfig_debug.dart", "/Users/donadev/Documents/Projects/AmmessoApp/lib/amplifyconfig_production.dart"]
-    const extension : string = "dart"//await question("Insert the file extension without the dot: ")
+    const folder : string = process.env.folder || await question("Insert the main folder where to search for strings: ")
+    const excluded_paths_str : string  = process.env.excluded_paths || await question("Insert the excluded paths, comma separated")
+    const excluded_paths = excluded_paths_str.split(",")
+    const extension : string = process.env.extension || await question("Insert the file extension without the dot: ")
+    const loco_api_key : string = process.env.loco_api_key || await question("Insert the Localise.biz API Key: ")
     let mode : string = await question("Select the script process to run: ([extract], upload, replace): ")
     if(mode == "") mode = "extract"
     switch(mode) {

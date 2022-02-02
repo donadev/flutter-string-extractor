1e33add1fbbffeaee936173451f4e69e0c4560e6
Luigi Donadel <luigi.donadel@gmail.com>
2022-02-02 12:41:15 +0100

    adds replacer logic

--- lib/replace.ts
+++ lib/replace.ts
@@ -1,23 +1,24 @@
 import * as fs from 'fs/promises';
 
 import { OutputString, OUTPUT_META_PATH as INPUT_META_PATH, OUTPUT_PATH as INPUT_PATH, OUTPUT_PATH } from "./extract";
+import { transformJson } from './upload';
 
 type ReplaceString = {from: string, to: string, file: string}
 
-const fileReader = async (path : string) : Promise<[string: any]> => {
+const fileReader = async (path : string, transformer : (string) => string = a => a) : Promise<[string: any]> => {
     const raw = await fs.readFile(path, "utf-8")
-    return JSON.parse(raw)
+    return JSON.parse(transformer(raw))
 }
 
 const getFile = async () : Promise<[string: string]> => {
-    return await fileReader(INPUT_PATH)
+    return await fileReader(INPUT_PATH, transformJson)
 }   
 
 const getMetaFile = async () : Promise<[string: [OutputString]]> => {
     return await fileReader(INPUT_META_PATH)
 }
 const transformKey = (key : string) : string => {
-    return `AppLocalizations.of(context)!.${key}`
+    return `AppLocalizations.of(context).${key}`
 }
 const transformMeta = (strings : [string: string], meta : [string: [OutputString]]) : ReplaceString[] => {
     return Object.values(meta).map(lines => {
@@ -32,7 +33,9 @@ const transformMeta = (strings : [string: string], meta : [string: [OutputString
 }
 
 const replace = async (data: ReplaceString) => {
-    //TODO implement
+    const content = await fs.readFile(data.file, "utf-8")
+    const edit = content.replace(data.from, data.to)
+    await fs.writeFile(data.file, edit, "utf-8")
 }
 
 const replacer = async () => {
@@ -40,7 +43,9 @@ const replacer = async () => {
     const meta = await getMetaFile()
     const replacePaths = transformMeta(strings, meta)
     console.log(replacePaths)
-    await Promise.all(replacePaths.map(replace))
+    for(const data of replacePaths) {
+        await replace(data)
+    }
 }
 
 export default replacer

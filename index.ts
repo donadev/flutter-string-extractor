c1de69b94c4b8825fbab057d84ad29d75df7d44a
Luigi Donadel <luigi.donadel@gmail.com>
2022-02-02 12:40:47 +0100

    adds exit condition

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import extractor from './lib/extract';
 import { question } from './lib/util/utils';
 import uploader from './lib/upload';
 import replacer from './lib/replace';
+import { exit } from 'process';
 
 
 
@@ -18,12 +19,16 @@ const start = async () => {
     let mode : string = await question("Select the script process to run: ([extract], upload, replace): ")
     if(mode == "") mode = "extract"
     switch(mode) {
-        case "extract": return await extractor(folder, extension, excluded_paths)
-        case "upload": return await uploader(loco_api_key)
-        case "replace": return await replacer()
-        default: return console.log("Not implemented yet")
+        case "extract": await extractor(folder, extension, excluded_paths)
+            break
+        case "upload": await uploader(loco_api_key)
+            break
+        case "replace": await replacer()
+            break
+        default: console.log("Not implemented yet")
     }
+    console.log("Script ended")
 }
 
 
-start().then(() => {})
+start().then(() => exit()).catch(console.error)

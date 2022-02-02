4e1fec1ca83ec0a23302b3db8e7baffb97d75179
Luigi Donadel <luigi.donadel@gmail.com>
2022-02-02 12:41:28 +0100

    upload camelcase keys

--- lib/upload.ts
+++ lib/upload.ts
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import * as camelcase from 'camelcase';
 import * as fs from 'fs/promises';
 import { OUTPUT_PATH as INPUT_PATH } from "./extract";
 
@@ -19,10 +20,22 @@ const upload = async (json : string, loco_api_key : string) => {
 const getJson = async () : Promise<string> => {
     return await fs.readFile(INPUT_PATH, "utf-8")
 }
+const transformKey = camelcase
+
+export const transformJson = (json : string) : string => {
+    const dict : {[id:string] : any[]} = JSON.parse(json)
+    const output : {[id:string] : any[]} = {}
+    Object.keys(dict).forEach(key => {
+        output[transformKey(key)] = dict[key]
+    })
+    return JSON.stringify(output)
+     
+}
 
 const uploader = async (apiKey : string) => {
     const json = await getJson()
-    await upload(json, apiKey)
+    const transformed = transformJson(json)
+    await upload(transformed, apiKey)
 }
 
 export default uploader;
